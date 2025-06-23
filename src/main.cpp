4598483ba76d930ecda7e665962dc5775487bfb2
Ali Arin Bulut <aliarinbulut@gmail.com>
2025-06-23 12:41:37 -0400

    Accessing the password file and checking if the file is there.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using std::getline, std::cin, std::cout, std::string;
+
+string path {""};
+std::fstream file {path, std::ios::in};
+
+bool Access_File(){
+    if (file.is_open())
+        return true;
+    else
+        return false;
+}
 
 int main(){
+    while(!Access_File()){
+        cout << "Please enter the path to the password file: ";
+        getline(cin, path);
+        file.open(path);
+        if (!Access_File())
+            cout << "Wrong File Path!\n";
+    }
 
 }
