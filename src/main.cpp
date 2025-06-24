52f3dfd4a0edddd78cc245865ba2c68caff79105
Ali Arin Bulut <aliarinbulut@gmail.com>
2025-06-23 22:09:33 -0400

    Added Check_Master_Key() Function

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,19 @@
 using std::getline, std::cin, std::cout, std::string;
 
 string path {""};
-std::fstream file {path, std::ios::in};
+std::fstream file;
 
 bool Access_File(){
-    if (file.is_open())
-        return true;
+    return file.is_open();
+}
+
+bool Check_Master_Key(){
+    file.clear();
+    file.seekg(0);
+
+    string first_line;
+    if (getline(file, first_line))
+        return first_line.find("Master Key:") != string::npos;
     else
         return false;
 }
@@ -17,6 +25,8 @@ int main(){
     while(!Access_File()){
         cout << "Please enter the path to the password file: ";
         getline(cin, path);
+        if (file.is_open())
+            file.close();
         file.open(path);
         if (!Access_File())
             cout << "Wrong File Path!\n";
